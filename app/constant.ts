2ecdcb4626271840165c077a0b9c8b1bc5c13406
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-08-08 22:15:02 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -255,22 +255,29 @@ export const KnowledgeCutOffDate: Record<string, string> = {
 
 const openaiModels = [
   "gpt-3.5-turbo",
+  "gpt-3.5-turbo-0301",
+  "gpt-3.5-turbo-0613",
   "gpt-3.5-turbo-1106",
   "gpt-3.5-turbo-0125",
+  "gpt-3.5-turbo-16k",
+  "gpt-3.5-turbo-16k-0613",
   "gpt-4",
+  "gpt-4-0314",
   "gpt-4-0613",
   "gpt-4-32k",
+  "gpt-4-32k-0314",
   "gpt-4-32k-0613",
-  "gpt-4-turbo",
+  "gpt-4-1106-preview",
+  "gpt-4-0125-preview",
   "gpt-4-turbo-preview",
+  "gpt-4-turbo",
+  "gpt-4-turbo-2024-04-09",
   "gpt-4o",
   "gpt-4o-2024-05-13",
   "gpt-4o-2024-08-06",
   "gpt-4o-mini",
   "gpt-4o-mini-2024-07-18",
   "gpt-4-vision-preview",
-  "gpt-4-turbo-2024-04-09",
-  "gpt-4-1106-preview",
   "dall-e-3",
 ];
 
@@ -355,17 +362,6 @@ export const DEFAULT_MODELS = [
       providerName: "OpenAI",
       providerType: "openai",
       sorted: 1, // 这里是固定的，确保顺序与之前内置的版本一致
-    },
-  })),
-  ...openaiModels.map((name) => ({
-    name,
-    available: true,
-    sorted: seq++,
-    provider: {
-      id: "azure",
-      providerName: "Azure",
-      providerType: "azure",
-      sorted: 2,
     },
   })),
   ...googleModels.map((name) => ({
