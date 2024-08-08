1e5aef4c3c940ce846b354a54ade28ac6e50a3ce
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-08-08 22:16:08 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "FREE ChatGPT-NextWeb",
+  description: "FREE ChatGPT-NextWeb.",
   appleWebApp: {
-    title: "NextChat",
+    title: "FREE ChatGPT-NextWeb",
     statusBarStyle: "default",
   },
 };
