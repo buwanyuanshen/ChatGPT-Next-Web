3d769e2de574e2f79b7f3697a0c5de2b437bf312
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-12-24 22:16:08 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -231,8 +231,14 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="ChatGPT-NextWeb"
+          subTitle={
+    <span
+      dangerouslySetInnerHTML={{
+        __html: `欢迎!购买APIKEY请前往<a href="https://api.gpt6.icu" target="_blank">CF-API</a>和<a href="https://shop.gpt6.icu" target="_blank">CF-SHOP</a>！获取更多体验请前往<a href="https://v2.gpt6.icu" target="_blank">CF-机场</a>、<a href="https://img.gpt6.icu" target="_blank">CF 图床</a>、<a href="https://gpt6.icu" target="_blank">Free Chat（后端）</a>、<a href="https://f1.gpt6.icu" target="_blank">Free Chat（前端）</a>、<a href="https://lobe.gpt6.icu" target="_blank">ChatGPT-Lobe</a>、<a href="https://mj.gpt6.icu" target="_blank">ChatGPT-MJ</a>、<a href="https://open.gpt6.icu" target="_blank">ChatGPT-OpenWebUI</a>、<a href="https://paint.gpt6.icu" target="_blank">Paint-Web</a>！欢迎加入q群226848325学习交流。`
+      }}
+    />
+  }
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
@@ -305,15 +311,6 @@ export function SideBar(props: { className?: string }) {
                   shadow
                 />
               </Link>
-            </div>
-            <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
-                <IconButton
-                  aria={Locale.Export.MessageFromChatGPT}
-                  icon={<GithubIcon />}
-                  shadow
-                />
-              </a>
             </div>
           </>
         }
